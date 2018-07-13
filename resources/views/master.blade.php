ba2ba42f7ccd02f856e90d1af14f7017bd72e811
ngocminh <minhln.ptit@gmail.com>
2018-07-13 10:54:06 +0700

    commit new project

---
+++ resources/views/master.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> @yield('title') </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+@include('shared.navbar')
+
+@yield('content')
+
+</body>
+</html>
